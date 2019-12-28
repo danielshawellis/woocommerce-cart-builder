2a06da11080c6752da9acc78f48c58e89d05cc5f
Daniel Ellis <danielshawellis@gmail.com>
2019-12-28 10:31:52 -0600

    Add plugin requirements and meta information in header

---
+++ woocommerce-cart-builder.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:       WooCommerce Cart Builder
+ * Description:       A pagebuilder plugin for the WooCommerce cart.
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
